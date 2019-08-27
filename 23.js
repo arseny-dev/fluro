4d57ca68f34e24b3015681b8e8295e58157a08bd
mac <mac@mac.local>
2019-08-27 22:45:14 +1000

    asdf

--- 23.js
+++ 23.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var Mailgun = require('mailgun-js');
 var app = express();
 const cors = require('cors');
 const logger = require('morgan');
 
+var api_key = '90533bcac8b17e228dcd5e6f6392b38c-898ca80e-39ae0c01';
+var domain = 'sandboxf7223b77d29e4d7f8a2876280ff01d3f.mailgun.org';
 app.use(
 	cors({
 		origin: '*',
@@ -15,28 +19,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/api/form', function(req, res) {
-	var nodemailer = require('nodemailer');
-	var transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 587,
-		secure: false, // secure:true for port 465, secure:false for port 587
-		auth: {
-			user: 'flurofast@gmail.com',
-			pass: 'Flurofast123'
-		}
-	});
-	var mailOptions = {
-		from: req.body.email,
-		to: 'flurofast@gmail.com',
+	console.log(req.body);
+	var mailgun = new Mailgun({ apiKey: api_key, domain: domain });
+	var data = {
+		from: 'Contact me <' + req.body.email + '>',
+		to: 'arseny910124@gmail.com',
 		subject: req.body.subject,
-		text: '<' + req.body.email + '> ' + req.body.name + ' sent ' + req.body.message
+		text: req.body.name + ' sent ' + req.body.message
 	};
-	console.log(mailOptions);
-	transporter.sendMail(mailOptions, function(error, info) {
-		if (error) {
-			console.log(error);
+	console.log(data);
+	mailgun.messages().send(data, function(err, body) {
+		if (err) {
+			//  res.render('error', { error: err });
+			res.send({ res: 'error' });
+			console.log('got an error: ', err);
 		} else {
-			console.log('Email sent: ' + info.response);
+			//  res.render('submitted', { email: req.params.mail });
+			res.send({ res: 'ok' });
 		}
 	});
 });
